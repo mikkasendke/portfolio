b2de309fd760b1b7c0df8288be5f2ef469a16d62
Mikka <mikka@sendke.eu>
2023-08-27 03:12:15 +0200

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,15 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Mikka Sendke</title>
+	<meta name="description" content="Portfolio website" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="h-[50vh] flex flex-col justify-center items-center bg-white dark:bg-gray-800">
+    <h1 class="text-7xl text-black dark:text-white">Mikka Sendke</h1>
+    <h2 class="text-4xl p-5 text-black dark:text-white">Software Engineer</h2>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,129 +1,39 @@
-<script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+<script lang="ts">
+	import { onMount } from "svelte";
+
+onMount(() => {
+    const html = document.querySelector('html');
+    if(!html) return;
+    const darkmode = localStorage.getItem('darkmode');
+    if(darkmode === 'true') {
+        toggleDarkMode();
+    }
+})
+
+let darkmodeSymbol = '🌙';
+function toggleDarkMode() {
+    const html = document.querySelector('html');
+    if(!html) return;
+    html.classList.toggle('dark');
+    if(html.classList.contains('dark')) {
+        darkmodeSymbol = '☀️';
+        localStorage.setItem('darkmode', 'true');
+    } else {
+        darkmodeSymbol = '🌙';
+        localStorage.setItem('darkmode', 'false');
+    }
+}
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
+<header class="flex items-center p-4 bg-gray-100 dark:bg-gray-700 text-black dark:text-white">
+    <a href="https://www.github.com/mikkasendke"><icon class="text-4xl select-none">👨‍💻</icon></a>
+    <nav class="flex justify-center flex-grow">
+        <ul class="text-xl">
+            <li class="inline p-2"><a href="/">Home</a></li>
+            <li class="inline p-2"><a href="/blog">Blog</a></li>
+            <li class="inline p-2"><a href="/cv">CV</a></li>
+        </ul>
+    </nav>
+    <button class="text-2xl" on:click={toggleDarkMode}>{darkmodeSymbol}</button>
 </header>
 
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
